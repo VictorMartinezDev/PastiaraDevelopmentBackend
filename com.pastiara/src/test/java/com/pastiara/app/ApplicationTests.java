fda750199c146b771c73884d100d80ef0a3d38ec
Victor Martinez <vicmanuelg19@gmail.com>
2025-10-15 16:07:39 -0600

    feat:primer commit de backend pastiara

---
+++ com.pastiara/src/test/java/com/pastiara/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastiara.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
